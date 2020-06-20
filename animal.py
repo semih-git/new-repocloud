fdfc16b0ecd251963270ec62ccb21d2ac565d58b
semih-git <cemcan34355@gmail.com>
2020-06-20 17:49:15 +0200

    animal.py tamamen guncellendi

--- animal.py
+++ animal.py
@@ -1,6 +1,15 @@
 def default():
-    print("helloooooooooo")
+    print("hello")
+def cat():
+    print("miyav")
+def dog():
+    print("hav")
 def main():
+    if sys.argv[1]=="dog":
+        dog()
+    elif sys.argv[1]=="cat":
+        cat()
+    else:
     default()
 if __name__=="__main__":
     main()
